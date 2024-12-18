d49de4573a40d1cabe0c61b9bd828e1f8b8b79ae
Joshua Nuckel <joshuanuckel@icloud.com>
2024-12-18 16:27:35 +0100

    Create fetch function

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchCharacters() {
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await response.json();
+  const characters = data.results;
+
+  characters.forEach((character) => {
+    const card = createCharacterCard(character);
+  });
+
+  return characters;
+}
+
+fetchCharacters();
